ca3d1e74121f1eee51a4e0cf822aa8100b5c11f6
FrodoLuo <frodoluo@outlook.com>
2018-11-05 19:49:49 +0800

    case fix

---
+++ src/components/header/index.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+import { SearchInput } from '../input-entities/Inputs';
+import './style.less';
+
+class Header extends React.Component {
+  public render() {
+    return (
+      <>
+        <header>
+          <div className="nav-wrapper">
+            <nav>
+              <NavLink exact={true} to="/">
+                <span>文章</span>
+              </NavLink>
+              <NavLink to="/album" activeClassName="active">
+                <span>照片</span>
+              </NavLink>
+              <NavLink to="/clipboard">
+                <span>留言板</span>
+              </NavLink>
+              <NavLink to="/aboutme">
+                <span>关于我</span>
+              </NavLink>
+            </nav>
+            <div className="search-wrapper">
+              <SearchInput />
+            </div>
+          </div>
+        </header>
+      </>
+    );
+  }
+  private searchBtn(e: React.ChangeEvent) {
+    console.log(e);
+  }
+  private searchKey(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'Enter') {
+      console.log('search:', e.currentTarget.value);
+    }
+
+  }
+}
+
+export default Header;
